9d848ba56b058df4bdd8708c3bbf7a2faeb2ffb3
anik <aniktitan@gmail.com>
2020-08-24 22:45:19 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+function breatheAnimation() {
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold On!';
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+breatheAnimation();
+
+setInterval(breatheAnimation, 7500);
